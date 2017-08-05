8b4af7be12207a1efe34cbd286879b645a0b045d
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-04 22:42:59 -0700

    add cocurrence matrix

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,9 +7,15 @@ public class Driver {
     public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
         String rawData = args[0];
         String userMovieListOutputDir = args[1];
+        String coCurrenceMatrixOutputDir = args[2];
+
         String[] path1 = {rawData, userMovieListOutputDir};
+        String[] path2 = {userMovieListOutputDir, coCurrenceMatrixOutputDir};
 
         DataDividedByUser dataDividedByUser = new DataDividedByUser();
         dataDividedByUser.main(path1);
+
+        CoCurrenceMatrix coCurrenceMatrix = new CoCurrenceMatrix();
+        coCurrenceMatrix.main(path2);
     }
 }
