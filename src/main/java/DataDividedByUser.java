e1d79e835d01b25d7aa228b527e5e9e3b53d6e20
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-04 21:49:33 -0700

    add data divideby use mapper class

--- src/main/java/DataDividedByUser.java
+++ src/main/java/DataDividedByUser.java
@@ -1,7 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
@@ -19,7 +24,22 @@ public class DataDividedByUser {
 //    public static class DataDivideReducer extends Reducer<> {
 //
 //    }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String inputDataDir = args[0];
+        String outputDataDir = args[1];
+
+        Configuration conf= new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setMapperClass(DataDivideMapper.class);
+//        job.setReducerClass(DataDivideReducer.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        TextInputFormat.setInputPaths(job, new Path(inputDataDir));
+        TextOutputFormat.setOutputPath(job, new Path(outputDataDir));
+        job.waitForCompletion(true);
     }
 }
