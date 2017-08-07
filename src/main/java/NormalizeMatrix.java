61980b1eccac558d428c71a20dee927275c78ac1
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-06 20:37:34 -0700

    find that problem, due to job.setOutputKeyClass(Text.class), if you do not set this type, its default setting is longwritable, outputValue is tex values.iterator().hasNext();

--- src/main/java/NormalizeMatrix.java
+++ src/main/java/NormalizeMatrix.java
@@ -5,22 +5,47 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zzc on 8/4/17.
  */
 
 public class NormalizeMatrix {
+    final static Logger logger = Logger.getLogger(NormalizeMatrix.class);
+
     public static class NormalizeMapper extends Mapper<LongWritable, Text, Text, Text> {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             System.out.println("norm"+value.toString());
-            super.map(key, value, context);
+            String[] array = value.toString().trim().split("-");
+            String movieId0 = array[0];
+            String movieId1_count = array[1];
+            context.write(new Text(movieId0), new Text(movieId1_count));
+        }
+    }
+
+    public static class NormalizeReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            while (values.iterator().hasNext()) {
+                String movie_count = values.iterator().next().toString();
+                String movie1 = movie_count.split("\t")[0];
+                int subCount = Integer.parseInt(movie_count.split("\t")[1]);
+                sum += subCount;
+                map.put(movie1, subCount);
+            }
+
         }
     }
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
@@ -36,10 +61,10 @@ public class NormalizeMatrix {
         Job job = Job.getInstance(configuration);
 
         job.setMapperClass(NormalizeMapper.class);
-
+        job.setReducerClass(NormalizeReducer.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
         TextInputFormat.setInputPaths(job, new Path(args[0]));
