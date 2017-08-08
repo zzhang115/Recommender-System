c46fd365992f69b36d44c3c5f07c6f153f62fdff
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-07 22:37:11 -0700

    multiplication map reduce done

--- src/main/java/ResultSum.java
+++ src/main/java/ResultSum.java
@@ -1,6 +1,7 @@
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,40 +15,33 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * Created by zzc on 8/7/17.
  */
+
 public class ResultSum {
     final static Logger logger = Logger.getLogger(NormalizeMatrix.class);
 
-    public static class NormalizeMapper extends Mapper<LongWritable, Text, Text, Text> {
+    public static class ResultSumMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            System.out.println("norm"+value.toString());
-            String[] array = value.toString().trim().split("-");
-            String movieId0 = array[0];
-            String movieId1_count = array[1];
-            context.write(new Text(movieId0), new Text(movieId1_count));
+            String[] array = value.toString().trim().split("\t");
+            context.write(new Text(array[0]), new DoubleWritable(Double.parseDouble(array[1])));
         }
     }
 
-    public static class NormalizeReducer extends Reducer<Text, Text, Text, Text> {
+    public static class ResultSumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
         @Override
-        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        protected void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
-            Map<String, Integer> map = new HashMap<String, Integer>();
-            while (values.iterator().hasNext()) {
-                String movie_count = values.iterator().next().toString();
-                String movie1 = movie_count.split("\t")[0];
-                int subCount = Integer.parseInt(movie_count.split("\t")[1]);
-                sum += subCount;
-                map.put(movie1, subCount);
+            for (DoubleWritable value : values) {
+                sum += value.get();
             }
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                context.write(key, new Text(entry.getKey() + ":" + (double) entry.getValue() / sum));
-            }
+            context.write(key, new DoubleWritable(sum));
         }
     }
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         String inputFileDir = args[0];
         String outputFileDir = args[1];
@@ -63,12 +57,12 @@ public class ResultSum {
         Configuration configuration = new Configuration();
         Job job = Job.getInstance(configuration);
 
-        job.setMapperClass(NormalizeMapper.class);
-        job.setReducerClass(NormalizeReducer.class);
+        job.setMapperClass(ResultSumMapper.class);
+        job.setReducerClass(ResultSumReducer.class);
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
 
         TextInputFormat.setInputPaths(job, new Path(inputFileDir));
         TextOutputFormat.setOutputPath(job, new Path(outputFileDir));
